d614be2768ba6823136d8ea135d4ecc85f9da65b
Ryohei Ikegami <iofg2100@gmail.com>
2023-02-04 01:36:30 +0900

    Add ObservableYMap

---
+++ packages/editor/src/utils/ObservableYMap.ts
@@ -0,0 +1,63 @@
+import { createAtom } from "mobx";
+import * as Y from "yjs";
+
+const instances = new WeakMap<Y.Map<any>, ObservableYMap<any>>();
+
+export class ObservableYMap<V> {
+  static get<V>(target: Y.Map<V>): ObservableYMap<V> {
+    let map = instances.get(target);
+    if (!map) {
+      map = new ObservableYMap(target);
+      instances.set(target, map);
+    }
+    return map;
+  }
+
+  readonly y: Y.Map<V>;
+  readonly _atom = createAtom("ObservableYMap");
+
+  private constructor(y: Y.Map<V>) {
+    this.y = y;
+    this.y.observe(() => {
+      this._atom.reportChanged();
+    });
+  }
+
+  get size(): number {
+    this._atom.reportObserved();
+    return this.y.size;
+  }
+
+  set(key: string, value: V): void {
+    this.y.set(key, value);
+  }
+
+  get(key: string): V | undefined {
+    this._atom.reportObserved();
+    return this.y.get(key);
+  }
+
+  has(key: string): boolean {
+    this._atom.reportObserved();
+    return this.y.has(key);
+  }
+
+  clear() {
+    this.y.clear();
+  }
+
+  keys(): IterableIterator<string> {
+    this._atom.reportObserved();
+    return this.y.keys();
+  }
+
+  values(): IterableIterator<V> {
+    this._atom.reportObserved();
+    return this.y.values();
+  }
+
+  [Symbol.iterator](): IterableIterator<[string, V]> {
+    this._atom.reportObserved();
+    return this.y[Symbol.iterator]();
+  }
+}
