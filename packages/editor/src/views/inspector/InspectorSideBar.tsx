51ba8150493f16853c5a1617b6ab875d4d5671f3
Ryohei Ikegami <iofg2100@gmail.com>
2023-02-10 21:51:44 +0900

    Remove right side bar tab

--- packages/editor/src/views/inspector/InspectorSideBar.tsx
+++ packages/editor/src/views/inspector/InspectorSideBar.tsx
@@ -1,36 +1,14 @@
 import { observer } from "mobx-react-lite";
-
 import { StyleInspector } from "./style/StyleInspector";
-import {
-  SideBarTabsContent,
-  SideBarTabsList,
-  SideBarTabsRoot,
-  SideBarTabsTrigger,
-} from "../../components/SideBarTabs";
 import { ScrollArea } from "../../components/ScrollArea";
 
 export const InspectorSideBar: React.FC = observer(() => {
   return (
-    <SideBarTabsRoot
-      defaultValue="design"
-      className="w-[232px] flex flex-col contain-strict"
-    >
-      <SideBarTabsList>
-        <SideBarTabsTrigger value="design">Design</SideBarTabsTrigger>
-        <SideBarTabsTrigger value="interaction">Interaction</SideBarTabsTrigger>
-      </SideBarTabsList>
-      <SideBarTabsContent
-        value="design"
-        className="flex-1 relative outline-none"
-      >
-        <ScrollArea className="absolute left-0 top-0 w-full h-full">
-          <StyleInspector />
-        </ScrollArea>
-      </SideBarTabsContent>
-      <SideBarTabsContent value="interaction">
-        <div className="p-3">Todo</div>
-      </SideBarTabsContent>
-    </SideBarTabsRoot>
+    <div className="w-[232px] flex flex-col contain-strict">
+      <ScrollArea className="absolute left-0 top-0 w-full h-full">
+        <StyleInspector />
+      </ScrollArea>
+    </div>
   );
 });
 InspectorSideBar.displayName = "InspectorSideBar";
