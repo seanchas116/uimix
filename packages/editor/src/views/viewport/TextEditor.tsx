dd7bd9590e06724eba827a9ac3bae911c8d66d93
Ryohei Ikegami <iofg2100@gmail.com>
2023-02-11 21:01:56 +0900

    Show at the same position

--- packages/editor/src/views/viewport/TextEditor.tsx
+++ packages/editor/src/views/viewport/TextEditor.tsx
@@ -1,20 +1,44 @@
 import { observer } from "mobx-react-lite";
+import { Selectable } from "../../models/Selectable";
+import { scrollState } from "../../state/ScrollState";
 import { viewportState } from "../../state/ViewportState";
+import { buildNodeCSS } from "./renderer/buildNodeCSS";
 
-export const TextEditor: React.FC = observer(() => {
-  const focused = viewportState.focusedSelectable;
-  if (!focus) {
-    return null;
-  }
+export const TextEditorBody: React.FC<{
+  selectable: Selectable;
+}> = observer(({ selectable }) => {
+  const cssStyle = buildNodeCSS("text", selectable.style);
+  const computedRect = selectable.computedRect;
+
   return (
     <div
       style={{
         position: "absolute",
-        left: 0,
-        top: 0,
+        transformOrigin: "left top",
+        transform: scrollState.documentToViewport.toCSSMatrixString(),
       }}
     >
-      {focused?.style.textContent}
+      <div
+        style={{
+          ...cssStyle,
+          position: "absolute",
+          left: computedRect.left + "px",
+          top: computedRect.top + "px",
+          width: computedRect.width + "px",
+          height: computedRect.height + "px",
+        }}
+        contentEditable
+      >
+        {selectable.style.textContent}
+      </div>
     </div>
   );
+});
+
+export const TextEditor: React.FC = observer(() => {
+  const focused = viewportState.focusedSelectable;
+  if (!focused) {
+    return null;
+  }
+  return <TextEditorBody selectable={focused} />;
 });
