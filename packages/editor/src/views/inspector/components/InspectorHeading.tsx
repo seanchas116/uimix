8c0849d0544f6661e2bf9fc77f1b8975f1663fad
Ryohei Ikegami <iofg2100@gmail.com>
2023-02-11 19:11:05 +0900

    Tweak

--- packages/editor/src/views/inspector/components/InspectorHeading.tsx
+++ packages/editor/src/views/inspector/components/InspectorHeading.tsx
@@ -24,7 +24,7 @@ export function InspectorHeading({
       >
         <Icon
           icon={icon}
-          className={twMerge("text-base opacity-60", iconClassName)}
+          className={twMerge("text-base opacity-80", iconClassName)}
         />
         {text}
       </h2>
