076cac386246dcda438914d8343d46fad96f2436
Ryohei Ikegami <iofg2100@gmail.com>
2023-02-19 19:23:39 +0900

    Disable proxy in production

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -2,26 +2,32 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/panel": "http://localhost:4000",
-      "/trpc": {
-        target: "ws://localhost:4000",
-        ws: true,
-      },
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      ...(mode === "development"
+        ? {
+            proxy: {
+              "/panel": "http://localhost:4000",
+              "/trpc": {
+                target: "ws://localhost:4000",
+                ws: true,
+              },
+            },
+          }
+        : {}),
     },
-  },
-  plugins: [
-    react({
-      babel: {
-        parserOpts: {
-          plugins: ["decorators-legacy"],
+    plugins: [
+      react({
+        babel: {
+          parserOpts: {
+            plugins: ["decorators-legacy"],
+          },
         },
-      },
-    }),
-  ],
-  build: {
-    target: "esnext",
-  },
+      }),
+    ],
+    build: {
+      target: "esnext",
+    },
+  };
 });
